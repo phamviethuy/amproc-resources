f76842c27c8ee521db5ea138bbbadd7b44fee534
huy <phamviethuy371@gmail.com>
2021-12-12 10:56:56 +0700

    update setting script

--- avr/configurations/update_setting.ps1
+++ avr/configurations/update_setting.ps1
@@ -3,20 +3,7 @@
 $myDocumentsPath = [Environment]::GetFolderPath("MyDocuments")
 $companyPath = $myDocumentsPath + "\amproc"
 $configPath = $companyPath + "\configurations"
-
-$currentFolder = [System.Environment]::CurrentDirectory + "\*"
-Write-Output "----------Chọn cài đặt mong muốn---------------"
-Write-Output "Chọn 2 nếu bạn thực sự hiểu lựa chọn này"
-Write-Output "1: Bỏ qua"
-Write-Output "2: Tạo mới Setting hoặc cập nhật cài đặt khi có cập nhật mới. Chỉ sử dụng khi lỗi phần mềm hoặc có yêu cầu từ vendor"
-
-$option = Read-Host -Prompt "Nhập lựa chọn"
-
-if($option -eq "2"){
-    stop-process -Id $PID
-}
-
-
+Write-Output "Update configurations"
 Write-Output "Start..."
 If (!(test-path $companyPath))
 {
